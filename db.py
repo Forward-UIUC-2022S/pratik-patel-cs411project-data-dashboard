1d6abce08fef9b7220d436d78f8505fbe206c8e0
Pratik Patel <pmp2@illinois.edu>
2022-03-18 12:57:45 -0500

    Added proper access to the database

--- db.py
+++ db.py
@@ -1,58 +1,22 @@
 import pandas as pd
 import os
+import zipfile
 
 
 class DB():
-    def __init__(self):
-        URL: str = "https://raw.githubusercontent.com/pratik139patell/CS411SP22-Data/main/"
-        
+    def __init__(self):        
         try:
             self.Affiliation_df: pd.DataFrame = pd.read_csv("Data/Affiliation.csv", index_col=False).sort_values(by=["Affiliation_name"])
-        except:
-            self.Affiliation_df: pd.DataFrame = self.get_df_from_url(URL + "Affiliation.csv").sort_values(by=["Affiliation_name"])
-            os.makedirs("Data", exist_ok=True)
-            self.Affiliation_df.to_csv("Data/Affiliation.csv", index=False)
-
-        try:
             self.Faculty_keyword_df: pd.DataFrame = pd.read_csv("Data/Faculty_keyword.csv", index_col=False)
-        except:
-            self.Faculty_keyword_df: pd.DataFrame = self.get_df_from_url(URL + "Faculty_keyword.csv")
-            self.Faculty_keyword_df.to_csv("Data/Faculty_keyword.csv", index=False)
-
-        try:
             self.Keyword_df: pd.DataFrame = pd.read_csv("Data/Keyword.csv", index_col=False).sort_values(by=["Keyword_name"])
-        except:
-            self.Keyword_df: pd.DataFrame = pd.read_csv(URL + "Keyword.csv", index_col=False).sort_values(by=["Keyword_name"])
-            self.Keyword_df.to_csv("Data/Keyword.csv", index=False)
-
-        try:
             self.Publication_keyword_df: pd.DataFrame = pd.read_csv("Data/Publication_keyword.csv", index_col=False)
-        except:
-            self.Publication_keyword_df: pd.DataFrame = pd.concat([self.get_df_from_url(URL + "Publication_keyword_"+str(i+1)+".csv") for i in range(4)])
-            self.Publication_keyword_df.to_csv("Data/Publication_keyword.csv", index=False)
-
-        try:
             self.Publication_df: pd.DataFrame = pd.read_csv("Data/Publication.csv", index_col=False)
-        except:
-            self.Publication_df: pd.DataFrame = pd.concat([self.get_df_from_url(URL + "Publication_"+str(i+1)+".csv") for i in range(4)])
-            self.Publication_df.to_csv("Data/Publication.csv", index=False)
-
-        try:
             self.Publish_df: pd.DataFrame = pd.read_csv("Data/Publish.csv", index_col=False)
-        except:
-            self.Publish_df: pd.DataFrame = self.get_df_from_url(URL + "Publish.csv")
-            self.Publish_df.to_csv("Data/Publish.csv", index=False)
-
-        try:
             self.Faculty_df: pd.DataFrame = pd.read_csv("Data/Faculty.csv", index_col=False).sort_values(by=["Faculty_name"])
         except:
-            self.Faculty_df: pd.DataFrame = self.get_df_from_url(URL + "Faculty.csv").sort_values(by=["Faculty_name"])
-            self.Faculty_df.to_csv("Data/Faculty.csv", index=False)
-
-    
-    def get_df_from_url(self, url) -> pd.DataFrame:
-        return pd.read_csv(url, index_col=False, on_bad_lines='skip')
-
+            with zipfile.ZipFile("assets\Data.zip", 'r') as zip_ref:
+                zip_ref.extractall("Data")
+                self.__init__()
 
 
     def get_affiliation_df(self) -> pd.DataFrame: return self.Affiliation_df
