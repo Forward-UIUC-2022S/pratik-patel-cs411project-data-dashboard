061d6acbd81ae1ee1e655c71580482f720a87840
Pratik Patel <pmp2@illinois.edu>
2022-04-05 12:13:04 -0500

    Updated dataframe to ignore casing

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
+from enum import Flag
 import pandas as pd
 import os
 import zipfile
 
 
 class DB():
-    def __init__(self):        
+    def __init__(self, flag=False):        
         try:
             self.Affiliation_df: pd.DataFrame = pd.read_csv("Data/Affiliation.csv", index_col=False).sort_values(by=["Affiliation_name"])
             self.Faculty_keyword_df: pd.DataFrame = pd.read_csv("Data/Faculty_keyword.csv", index_col=False)
@@ -13,10 +14,21 @@ class DB():
             self.Publication_df: pd.DataFrame = pd.read_csv("Data/Publication.csv", index_col=False)
             self.Publish_df: pd.DataFrame = pd.read_csv("Data/Publish.csv", index_col=False)
             self.Faculty_df: pd.DataFrame = pd.read_csv("Data/Faculty.csv", index_col=False).sort_values(by=["Faculty_name"])
+
+            if flag:
+                for data_frame in [self.Affiliation_df, self.Keyword_df, self.Publication_df, self.Faculty_df]:
+                    for col in data_frame.columns:
+                        if "photoUrl" in col: continue
+                        try: data_frame[col] = data_frame[col].apply(lambda x: x.title()) 
+                        except: continue
+                self.save_affiliation_df()
+                self.save_faculty_df()
+                self.save_keyword_df()
+                self.save_publication_df()
         except:
             with zipfile.ZipFile("assets\Data.zip", 'r') as zip_ref:
                 zip_ref.extractall("Data")
-                self.__init__()
+                self.__init__(True)
 
 
     def get_affiliation_df(self) -> pd.DataFrame: return self.Affiliation_df
@@ -26,6 +38,35 @@ class DB():
     def get_publication_keyword_df(self) -> pd.DataFrame: return self.Publication_keyword_df
     def get_publication_df(self) -> pd.DataFrame: return self.Publication_df
     def get_publish_df(self) -> pd.DataFrame: return self.Publish_df
+
+
+    def save_affiliation_df(self) -> None:
+        os.makedirs("Data", exist_ok=True)
+        self.Affiliation_df.to_csv("Data/Affiliation.csv", index=False)
+
+    def save_faculty_keyword_df(self) -> None:
+        os.makedirs("Data", exist_ok=True)
+        self.Faculty_keyword_df.to_csv("Data/Faculty_keyword.csv", index=False)
+
+    def save_faculty_df(self) -> None:
+        os.makedirs("Data", exist_ok=True)
+        self.Faculty_df.to_csv("Data/Faculty.csv", index=False)
+
+    def save_keyword_df(self) -> None:
+        os.makedirs("Data", exist_ok=True)
+        self.Keyword_df.to_csv("Data/Keyword.csv", index=False)
+
+    def save_publication_keyword_df(self) -> None:
+        os.makedirs("Data", exist_ok=True)
+        self.Publication_keyword_df.to_csv("Data/Publication_keyword.csv", index=False)
+
+    def save_publication_df(self) -> None:
+        os.makedirs("Data", exist_ok=True)
+        self.Publication_df.to_csv("Data/Publication.csv", index=False)
+        
+    def save_publish_df(self) -> None: 
+        os.makedirs("Data", exist_ok=True)
+        self.Publish_df.to_csv("Data/Publish.csv", index=False)
 
 
     def set_affiliation_df(self, df: pd.DataFrame) -> None: 
